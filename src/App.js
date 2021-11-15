6a8deeb717a010712f617887e3e16d1cabd4bb67
amitb26 <amitb3460@gmail.com>
2021-11-15 23:56:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./MyComponents/Header";
+import { Footer } from "./MyComponents/Footer";
+import { Todos } from "./MyComponents/Todos";
+import { AddTodo } from "./MyComponents/AddTodo";
+import {About} from "./MyComponents/About";
+import { useState, useEffect } from "react";
+
+import { 
+  BrowserRouter as Router,
+  Switch,
+   Route,  
+  } from "react-router-dom";
+function App() {
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  } else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+  const addTodo = (title, desc) => {
+    console.log("I am adding this todo", title, desc);
+    let sno;
+    if (todos.length===0) {
+      sno = 0;
+    } else {
+      sno = todos[todos.length - 1].sno + 1;
+    }
+
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    };
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+  };
+  const onDelete = (todo) => {
+    console.log("I am ondelete of todo", todo);
+    //let index = todo.indexOf(todo);
+    //todos.splice(index,1);
+    setTodos(
+      todos.filter((e) => {
+        return e !== todo;
+      })
+    );
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+  const [todos, setTodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+  return (
+    <>
+      <Router>
+        <Header title="My Todos List" searchBar={true} />
+        <Switch>
+          <Route
+            exact path="/"
+            render={() => {
+              return (
+                <>
+                  <AddTodo addTodo={addTodo} />
+                  <Todos todos={todos} onDelete={onDelete} />
+                </>
+              );
+            }}
+          >
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </Router>
+    </>
+  );
+}
+
+export default App;
